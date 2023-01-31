563ed5d2ec00a981143a35a8c827bfeaa038a418
Erik Henrique <erickhenrique110410@gmail.com>
2023-01-30 21:53:53 -0300

    Desafio hotel concluido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "654321", modelo: "Modelo 01", imei: "123",memoria: 254);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("--------------------------------------------------");
+
+Console.WriteLine("Smartphone iphone:");
+Iphone iphone = new Iphone(numero: "123456", modelo: "Modelo 02", imei: "321",memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tiktok");
+
+
+
